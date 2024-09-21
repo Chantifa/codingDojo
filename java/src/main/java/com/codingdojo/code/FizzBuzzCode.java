56d8f71ef7ab7f7d0a7a93676289efe01bd8813a
chantifa <chantifa@windowslive.com>
2024-09-21 15:03:57 +0200

    coded without functions or methode... empty codes

--- java/src/main/java/com/codingdojo/code/FizzBuzzCode.java
+++ java/src/main/java/com/codingdojo/code/FizzBuzzCode.java
@@ -5,23 +5,4 @@ public class FizzBuzzCode {
     public FizzBuzzCode() {
     }
 
-    public void playFizzBuzz(int start, int end) {
-
-    }
-
-    public String getFizzBuzzValue(int i) {
-
-    }
-
-    private boolean isFizz(int i) {
-
-    }
-
-    private boolean isBuzz(int i) {
-
-    }
-
-    private boolean containsDigit(int number, int digit) {
-
-    }
 }
--- python/com/codingdojo/code/fizz_buzz.py
+++ python/com/codingdojo/code/fizz_buzz.py
@@ -1,22 +1,3 @@
 from io import StringIO
 import sys
 
-# Function to check if a number is divisible by 3
-def is_fizz(n):
-
-
-# Function to check if a number is divisible by 5
-def is_buzz(n):
-
-
-# Function to get the FizzBuzz value for a single number
-def get_fizz_buzz_value(n):
-
-
-# Function to play the FizzBuzz game from start to end
-def fizz_buzz(start, end):
-
-
-# Helper function to capture the output of the fizz_buzz function
-def capture_output(start, end):
-
